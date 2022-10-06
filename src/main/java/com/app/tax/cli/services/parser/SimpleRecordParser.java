fa4934bfd9790d1c03834c6f08ecfd1c029762f8
Deni Permana <i.denipermana@gmail.com>
2022-10-06 15:58:59 +0700

    remove workspace idea

--- src/main/java/com/app/tax/cli/services/parser/SimpleRecordParser.java
+++ src/main/java/com/app/tax/cli/services/parser/SimpleRecordParser.java
@@ -7,7 +7,7 @@ public class SimpleRecordParser implements RecordParser {
 
     @Override
     public double getRecordTaxAmount(Transaction trx, int userId, TaxType taxType) {
-        if (trx.getTaxType() == taxType && trx.getCustomerId() == userId) {
+        if (isValid(trx, userId, taxType)) {
             return trx.getAmount() * 0.1;
         }
 
@@ -16,9 +16,13 @@ public class SimpleRecordParser implements RecordParser {
 
     @Override
     public double getAmount(Transaction trx, int userId, TaxType taxType) {
-        if (trx.getTaxType() == taxType && trx.getCustomerId() == userId) {
+        if (isValid(trx, userId, taxType)) {
             return trx.getAmount();
         }
         return 0;
+    }
+
+    private boolean isValid(Transaction trx, int userId, TaxType taxType) {
+        return trx.getTaxType() == taxType && trx.getCustomerId() == userId;
     }
 }
